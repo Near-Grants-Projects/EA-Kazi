4cf19e782bb2f12c12b58d4854bb8a7d3f1f599a
Ademayowa <ademayowaadeniyi@gmail.com>
2022-05-25 14:55:46 +0100

    added login page

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,13 +1,49 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Col, Form, Group, Label, Button } from 'react-bootstrap';
+import Link from 'next/link';
+import Layout from '@/components/Layout';
+import CardTitle from './cardTitle';
 
 export default function Login() {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h2>Login page</h2>
-        </Col>
-      </Row>
-    </Container>
+    <Layout title='Sign In'>
+      <div className='login'>
+        <Container>
+          <Col lg={6} className='mx-auto p-4'>
+            <div className='card card-body shadow-sm p-4'>
+              <CardTitle
+                title='Sign In'
+                text='Sign in to your EA Kazi account'
+              />
+              <Form>
+                <Form.Group className='mb-4'>
+                  <Form.Control type='email' placeholder='Email address' />
+                </Form.Group>
+
+                <Form.Group className='mb-4'>
+                  <Form.Control type='password' placeholder='Password' />
+                </Form.Group>
+                <div className='mb-3 d-flex justify-content-end'>
+                  <Link href='#'>
+                    <a className='text-secondary'> Forgot your password?</a>
+                  </Link>
+                </div>
+
+                <div className='d-grid gap-2'>
+                  <Button size='lg' className='fs-6'>
+                    Sign in
+                  </Button>
+                  <p className='text-secondary text-center'>
+                    Don't have an account?
+                    <Link href='/account/register'>
+                      <a className='ms-1'>Sign up</a>
+                    </Link>
+                  </p>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Container>
+      </div>
+    </Layout>
   );
 }
--- pages/account/register.js
+++ pages/account/register.js
@@ -15,27 +15,27 @@ export default function Register() {
                 text='Sign up for an EA Kazi account'
               />
               <Form>
-                <Form.Group className='mb-3'>
+                <Form.Group className='mb-4'>
                   <Form.Control type='text' placeholder='Full name' />
                 </Form.Group>
 
-                <Form.Group className='mb-3'>
+                <Form.Group className='mb-4'>
                   <Form.Control type='email' placeholder='Email address' />
                 </Form.Group>
 
-                <Form.Select className='mb-3 fs-6 text-secondary' size='lg'>
+                <Form.Select className='mb-4 fs-6 text-secondary' size='lg'>
                   <option>Sign up as</option>
                   <option>Employer</option>
                   <option>Trainer</option>
                 </Form.Select>
 
-                <Form.Group className='mb-3'>
+                <Form.Group className='mb-4'>
                   <Form.Control type='password' placeholder='Password' />
                 </Form.Group>
 
                 <div className='d-grid gap-2 '>
                   <Button size='lg' className='fs-6'>
-                    create an account
+                    Create an account
                   </Button>
                   <p className='text-secondary text-center'>
                     Already have an account?
